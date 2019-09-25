f6a1aa4fea8c385732db08fcef78397e21843ee9
River Phillips <riverphillips1@gmail.com>
2019-09-25 18:22:08 +0100

    Add create, update, delete endpoints
    Signed-off-by: River Phillips <riverphillips1@gmail.com>

--- src/main/scala/io/github/RiverPhillips/http4stodo/repository/TodoRepository.scala
+++ src/main/scala/io/github/RiverPhillips/http4stodo/repository/TodoRepository.scala
@@ -10,13 +10,38 @@ class TodoRepository(transactor: Transactor[IO]) {
     Todo(Some(1), "test")
   )
 
-  def getTodos(): fs2.Stream[IO, Todo] =
+  def getTodos: fs2.Stream[IO, Todo] =
     sql"SELECT id, description FROM todo".query[Todo].stream.transact(transactor)
 
   def getTodoById(id: Long): IO[Either[TodoNotFoundError.type, Todo]] = {
     sql"SELECT id, description FROM todo WHERE id = $id".query[Todo].option.transact(transactor).map{
       case Some(todo) => Right(todo)
       case _ => Left(TodoNotFoundError)
+    }
+  }
+
+  def createTodo(todo: Todo): IO[Todo] = {
+    sql"INSERT INTO todo (description) VALUES (${todo.description})".update.withUniqueGeneratedKeys[Long]("id").transact(transactor).map{id =>
+      todo.copy(id = Some(id))
+    }
+  }
+
+  def updateTodo(id: Long, todo: Todo): IO[Either[TodoNotFoundError.type, Todo]] = {
+    sql"UPDATE todo SET description = ${todo.description} WHERE  id = $id".update.run.transact(transactor).map{affectedRows =>
+      if(affectedRows == 1){
+        Right(todo.copy(id = Some(id)))
+      }else{
+        Left(TodoNotFoundError)
+      }
+    }
+  }
+
+  def deleteTodo(id: Long): IO[Either[TodoNotFoundError.type, Unit]] = {
+    sql"DELETE FROM todo WHERE id = $id".update.run.transact(transactor).map {affectedRows =>
+      if(affectedRows  == 1)
+        Right(())
+      else
+        Left(TodoNotFoundError)
     }
   }
 }
--- src/main/scala/io/github/RiverPhillips/http4stodo/service/TodoService.scala
+++ src/main/scala/io/github/RiverPhillips/http4stodo/service/TodoService.scala
@@ -3,27 +3,56 @@ package io.github.RiverPhillips.http4stodo.service
 import cats.effect.IO
 import io.circe.generic.auto._
 import io.circe.syntax._
+import io.github.RiverPhillips.http4stodo.models.Todo
 import io.github.RiverPhillips.http4stodo.repository.TodoRepository
-import org.http4s.{HttpRoutes, MediaType}
+import org.http4s.circe.CirceEntityDecoder._
 import org.http4s.circe._
 import org.http4s.dsl._
-import org.http4s.implicits._
 import org.http4s.headers.`Content-Type`
+import org.http4s.implicits._
+import org.http4s.{HttpRoutes, MediaType}
 
 
 class TodoService(todoRepository: TodoRepository) extends Http4sDsl[IO] {
-  val service  = HttpRoutes.of[IO]{
+  val service = HttpRoutes.of[IO] {
     case GET -> Root / "todos" =>
       val todoStream = fs2.Stream("[") ++ todoRepository.getTodos.map(_.asJson.noSpaces).intersperse(",") ++ fs2.Stream("]")
       Ok(todoStream, `Content-Type`(new MediaType("application", "json")))
 
     case GET -> Root / "todos" / LongVar(id) =>
-      for{
-        getResult <- todoRepository.getTodoById(id)
-        response <- getResult match {
+      for {
+        result <- todoRepository.getTodoById(id)
+        response <- result match {
           case Right(todo) => Ok(todo.asJson)
           case Left(_) => NotFound()
         }
-      }yield response
+      } yield response
+
+    case req@POST -> Root / "todos" =>
+      for {
+        todo <- req.as[Todo]
+        result <- todoRepository.createTodo(todo)
+        response <- Ok(result.asJson)
+      } yield response
+
+    case req@PUT -> Root / "todos" / LongVar(id) =>
+      for {
+        todo <- req.as[Todo]
+        result <- todoRepository.updateTodo(id, todo)
+        response <- result match {
+          case Right(todo) => Ok(todo.asJson)
+          case Left(_) => NotFound()
+        }
+      } yield response
+
+    case DELETE -> Root / "todos" / LongVar(id) =>
+      for {
+        result <- todoRepository.deleteTodo(id)
+        response <- result match {
+          case Right(_) => NoContent()
+          case Left(_) => NotFound()
+        }
+      } yield response
+
   }.orNotFound
 }
